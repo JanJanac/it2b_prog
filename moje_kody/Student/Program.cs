903de622cd55a8beb54194738bb93aba208b3e0d
Tvé jméno <tvuj@email.cz>
2026-04-23 09:35:52 +0200

    kód

---
+++ moje_kody/Student/Program.cs
@@ -0,0 +1,17 @@
+﻿class Program
+{
+    static void Main()
+    {
+        Student Borivoj = new Student("Borivoj", 67, "IT69");
+
+        Console.WriteLine("Jmeno: " + Borivoj.Jmeno);
+
+        Borivoj.PridatZnamku(6);
+        Borivoj.PridatZnamku(7);
+
+        Borivoj.VypisZnamky();
+
+        Student Patrick = new Student("Patick", 15, "IT-1A");
+    }
+    
+}
---
+++ moje_kody/Student/Student.cs
@@ -0,0 +1,69 @@
+﻿public class Student
+{
+    public string Jmeno { get; set; }
+    public int Vek { get; set; }
+    public string Trida { get; set; }
+
+    private List<int> Znamky;
+
+    public Student(string jmeno, int vek, string trida)
+    {
+        Jmeno = jmeno;
+        Vek = vek;
+        Trida = trida;
+        Znamky = new List<int>();
+    }
+
+    public void PridatZnamku(int novaZnamka)
+    {
+        Znamky.Add(novaZnamka);
+    }
+
+    public void VypisZnamky()
+    {
+        Console.WriteLine("Znamky studenta: ");
+        foreach (int z in Znamky)
+        {
+            Console.WriteLine("   " + z);
+        }
+    }
+
+    public void SmazatZnamku(int Znamka)
+    {
+        if (Znamky.Contains(Znamka))
+        {
+            Znamky.Remove(Znamka);
+        }
+    }
+
+    public double Prumer()
+    {
+        if (Znamky.Count == 0)
+        {
+            return 0;
+        }
+
+        int soucet = 0;
+
+        foreach (int z in Znamky)
+        {
+            soucet += z;
+        }
+
+        return (double) soucet / Znamky.Count;
+    }
+
+    public void VypisInfo()
+    {
+        Console.WriteLine($"Jmeno: {Jmeno} ");
+        Console.WriteLine($"Vek: {Vek} ");
+        Console.WriteLine($"Trida: {Trida} ");
+        Console.WriteLine($"Prumer: {Prumer()} ");
+
+        Console.WriteLine("Znamky:");
+        foreach (int z in Znamky)
+        {
+            Console.WriteLine("   " + z);
+        }
+    }
+}
